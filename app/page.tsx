75f644daed9e7a0524e0ce10846473e9559ac5d6
vinicsperes <peresvini2@gmail.com>
2024-11-05 21:46:27 -0300

    Tailwind Test

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
-function Home() {
+export default function Home() {
   return (
-    <h1>Home</h1>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="text-center p-6 bg-white rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold">Hello, World!</h1>
+        <p className="mt-2 text-gray-600">Thats a div!</p>
+      </div>
+    </div>
   );
 }
-export default Home;
